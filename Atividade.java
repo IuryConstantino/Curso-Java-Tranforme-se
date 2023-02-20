e1fb46a6ef9d57085023a7e27702348e569449b6
IuryConstantino <121960144+IuryConstantino@users.noreply.github.com>
2023-02-20 16:32:10 -0300

    Add files via upload

---
+++ Atividade.java
@@ -0,0 +1,16 @@
+
+        package Atividade;
+
+        import javax.swing.JOptionPane;
+        
+        public class Atividade {
+            public static void main(String[] args) {
+                double valor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do produto: "));
+                int desconto = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do desconto: "));
+                double total = valor * desconto / 100;
+                total = valor - total;
+                JOptionPane.showMessageDialog(null, "O valor a pagar é : " + total);
+            }
+            
+        }
+        
